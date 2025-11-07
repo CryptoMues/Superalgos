5c217fefa350e96751e011cb15c31c0aab48d148
Kaustav Chakraborty <deven.ferven@gmail.com>
2025-11-07 23:53:43 +0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,4 @@
+
+
+If you want to build something more than CLI, this gives inspiration.
+How you might use: Modify UI skin + add your branding “CryptoCCS” and publish an extended version or plugin.
